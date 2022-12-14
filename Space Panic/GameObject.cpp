6a0755fa867d004a0cdaa58c6b5d25d32c22d8a1
Panzer0312 <dddrechsler@web.de>
2022-12-14 01:51:03 +0100

    Added Comments to GameObject

--- Space Panic/GameObject.cpp
+++ Space Panic/GameObject.cpp
@@ -1,81 +1,72 @@
 #include "GameObject.h"
-//USE FOR ENEMY ETC STORAGE
 
-
+/**
+ * .
+ * Dummy constructor
+ */
 GameObject::GameObject()
 {
+	type = BRICK;
+	pos = Vector2f(0,0);
+	speed = -1;
+	spriteID = -1;
+	name = "";
+	animate = false;
+	facing = Vector2i(0,0);
 }
 
+/**
+ * .
+ * 
+ * \param objSpriteID ID of the associated Sprite to the GameObject 
+ * \param objName Name of the GameObject to find it easier in Objects vector in GameModel
+ * \param objPos Position where the Sprite of the GameObject is drawn
+ * \param objSpeed Speed with which Object will be moved
+ * \param objType Type of a GameObject to differentiate those in the Model's vector Objects
+ */
 GameObject::GameObject(int objSpriteID, std::string objName, Vector2f objPos, float objSpeed, objectType objType){
 	spriteID = objSpriteID;
 	name = objName;
 	pos = objPos;
 	speed = objSpeed;
 	type = objType;
-	facing = Vector2i(0, 0);
-	//animate = false;
 }
-
+/** setter method */
 void GameObject::setPos(Vector2f position) {
 	pos = position;
 }
-
+/** getter method */
 int GameObject::getID()
 {
 	return spriteID;
 }
-
+/** getter method */
 Vector2f GameObject::getPos()
 {
 	return pos;
 }
-
+/** getter method */
 objectType GameObject::getType()
 {
 	return type;
 }
-
+/** getter method */
 float GameObject::getSpeed()
 {
 	return speed;
 }
-
+/** getter method */
 std::string GameObject::getName()
 {
 	return name;
 }
-
+/** getter method */
 Vector2i GameObject::getFacing()
 {
 	return facing;
 }
-
+/** setter method */
 void GameObject::setFacing(Vector2i dir)
 {
 	facing = dir;
 }
-
-/*
-bool GameObject::isAnimated()
-{
-	return animate;
-}
-
-void GameObject::setAnimated(bool a)
-{
-	animate = a;
-}
-
-std::vector<Vector2i> GameObject::getAnimations() {
-	return animations;
-}
-
-
-float GameObject::getAnimationSpeed() {
-	return animationSpeed;
-}
-
-void GameObject::setAnimationSpeed(float newSpeed) {
-	animationSpeed = newSpeed;
-}
-*/
--- Space Panic/GameObject.h
+++ Space Panic/GameObject.h
@@ -5,22 +5,26 @@
 class GameObject
 {
 public:
+	/** Empty Constructor of a dummy GameObject */
 	GameObject();
+	/** Contructor for a new GameObject */
 	GameObject(int objSpriteID, std::string objName, Vector2f objPos, float objSpeed, objectType objType);
-//	GameObject(int objSpriteID, std::string objName, Vector2f objPos, float objSpeed, objectType objType, std::vector<Vector2i> animationsArray, float animSpeed);
+	/** Sets the Position of a GameObject to reference position where it's sprite is drawn on the screen positon */
 	void setPos(Vector2f pos);
-//	void changeSprite(int width, Vector2i pos);
+	/** returns ID of the GameObject's sprite */
 	int getID();
+	/** returns postion of a GameObject*/
 	Vector2f getPos();
+	/** returns type of a GameObject to differentiate those in the Model's vector Objects */
 	objectType getType();
+	/** returns speed how fast the GameObject should be moved */
 	float getSpeed();
+	/** returns name of the specific GameObject */
 	std::string getName();
+	/** returns the position where the Object is facing to */
 	Vector2i getFacing();
+	/** sets the position where the Object is facing to */
 	void setFacing(Vector2i dir);
-
-//	bool isAnimated();
-//	void setAnimated(bool a);
-
 
 private:
 	objectType type;
